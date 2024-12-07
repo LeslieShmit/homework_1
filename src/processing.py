254b997d7382e882f60566da74f85300fb4a1afb
Vladislav Lukin <vladislavlukin090392@gmail.com>
2024-12-07 21:02:10 +0400

    fixes

--- src/processing.py
+++ src/processing.py
@@ -9,8 +9,9 @@ def filter_by_state(list_of_dicts: list, chosen_state: str = "EXECUTED") -> list
     return new_list_of_dicts
 
 
-def sort_by_date(list_of_dicts: list, sort_order_is_reverse: bool = True) -> list:
+def sort_by_date(list_of_dicts: list, is_reverse: bool = True) -> list:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание) и возвращает новый список, отсортированный по дате (date)."""
-    sorted_list = sorted(list_of_dicts, key=lambda x: x["date"], reverse=sort_order_is_reverse)
+    sorted_list = sorted(list_of_dicts, key=lambda dictionary: dictionary["date"], reverse=is_reverse)
     return sorted_list
+
