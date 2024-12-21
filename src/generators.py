09b4eeb143d3c77fc1d3c1cc9a4207421b354acb
Vladislav Lukin <vladislavlukin090392@gmail.com>
2024-12-21 19:26:22 +0400

    fixes

--- src/generators.py
+++ src/generators.py
@@ -2,6 +2,8 @@ from mypy.server.objgraph import Iterable
 
 
 def filter_by_currency(list_of_operations: list[dict], currency: str) -> Iterable:
+    """Функция принимает на вход список словарей, представляющих транзакции и возвращает итератор, который поочередно
+    выдает транзакции, где валюта операции соответствует заданной"""
     try:
         for dictionary in list_of_operations:
             value = dictionary["operationAmount"]["currency"]["code"]  # noqa: F841
