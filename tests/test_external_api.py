8f486d6882804c81f295f923cb414079a43ea3b0
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-01-05 19:45:40 +0400

    add tests for func convertion_to_rubles

---
+++ tests/test_external_api.py
@@ -0,0 +1,56 @@
+import pytest
+from unittest import mock
+from unittest.mock import patch
+
+from src.external_api import convertion_to_rubles
+
+
+@patch("requests.request")
+def test_convertion_to_rubles(mock_request):
+    mock_request.return_value.json.return_value = {
+  "date": "2018-02-22",
+  "historical": "",
+  "info": {
+    "rate": 148.972231,
+    "timestamp": 1519328414
+  },
+  "query": {
+    "amount": 1,
+    "from": "USD",
+    "to": "RUB"
+  },
+  "result": 100.12,
+  "success": True
+}
+    assert convertion_to_rubles({
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "1",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }) == 100.12
+
+def test_convertion_rubles_to_rubles():
+    assert convertion_to_rubles({
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }) == 31957.58
