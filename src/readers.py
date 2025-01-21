c622562b1748e3444724f3e85c6d1b85db135767
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-01-21 22:28:41 +0400

    add func transaction_reader_exel

--- src/readers.py
+++ src/readers.py
@@ -7,3 +7,9 @@ def transaction_reader_csv(path_to_file: str) -> list[dict]:
     transaction_df = pd.read_csv(path_to_file, sep=";")
     transaction_list = transaction_df.to_dict("records")
     return transaction_list
+
+def transaction_reader_exel(path_to_file: str) -> list[dict]:
+    """Функция принимает на вход путь до файла exel и возвращает содержимое в виде списка словарей."""
+    transaction_data_exel = pd.read_excel(path_to_file)
+    transaction_list = transaction_data_exel.to_dict("records")
+    return transaction_list
